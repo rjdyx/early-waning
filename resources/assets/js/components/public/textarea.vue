87dfcaf08f425af61f247049fbb4d720ce3e39da
rjdyx <1191200688@qq.com>
2017-03-13 11:48:13 +0800

    [ADD]补上textarea.vue组件

---
+++ resources/assets/js/components/public/textarea.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-input type="textarea" v-model="content" id="content"></el-input>
+</template>
+
+<script>
+
+    export default {
+        name:'Textarea',
+        props: {
+            value: 0
+        },
+        data () {
+            return {
+                content: this.value
+            }
+        },
+        watch: {
+            content (){
+                this.$emit('return-value', {name: 'content', value: this.content})
+            }
+        }
+
+    }
+</script>
